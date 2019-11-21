d6c87844c55a9399964ed3d94973e07e58505617
Bowen Liu <45830436+BowenLiu92@users.noreply.github.com>
2019-11-20 20:55:12 -0500

    Add files via upload

---
+++ The Supermarket Queue.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def queue_time(customers, n):
+    d = {}
+    if not customers: return 0
+    if n > sum(customers): return max(customers)
+    for i in range(n):
+        d[i] = customers[i]
+    for i in range(n, len(customers)):
+        d[min(d, key = d.get)] += customers[i]
+    return max(d.values())
+
