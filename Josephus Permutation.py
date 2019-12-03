d0a1a4649b2d0381b31614158211f9ca5dba485b
Bowen Liu <45830436+BowenLiu92@users.noreply.github.com>
2019-12-03 07:53:12 -0500

    Update Josephus Permutation.py

--- Josephus Permutation.py
+++ Josephus Permutation.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (default, Aug  9 2019, 18:34:13) [MSC v.1915 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def josephus(items,k):
+def josephus(items,k):
     res = []
     i = 0
     while items:
