b52ae4f50a18783b1cae571e50bcae4b49811bcf
Bowen Liu <45830436+BowenLiu92@users.noreply.github.com>
2019-12-03 16:02:11 -0500

    Update Jimmy's Journey.py

--- Jimmy's Journey.py
+++ Jimmy's Journey.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (default, Aug  9 2019, 18:34:13) [MSC v.1915 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def ETA(path):
+def ETA(path):
     d = {'-':1, '/':2, '|':0.5}
     res = 0
     for i in path:
