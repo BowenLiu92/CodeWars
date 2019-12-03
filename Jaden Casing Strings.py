3a902589fb2b5f8a6f0b629778d255ab511a6b11
Bowen Liu <45830436+BowenLiu92@users.noreply.github.com>
2019-12-03 16:33:07 -0500

    Update Jaden Casing Strings.py

--- Jaden Casing Strings.py
+++ Jaden Casing Strings.py
@@ -1,4 +1,2 @@
-Python 3.7.4 (default, Aug  9 2019, 18:34:13) [MSC v.1915 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def toJadenCase(string):
+def toJadenCase(string):
     return ' '.join([ele.capitalize() for ele in string.split()])
