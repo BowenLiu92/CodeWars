d822bb8d11037db273e3ab50e77bebc055b016c0
Bowen Liu <45830436+BowenLiu92@users.noreply.github.com>
2019-12-04 09:25:08 -0500

    Update The Deaf Rats of Hamelin.py

--- The Deaf Rats of Hamelin.py
+++ The Deaf Rats of Hamelin.py
@@ -1,4 +1,2 @@
-Python 3.7.4 (default, Aug  9 2019, 18:34:13) [MSC v.1915 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def count_deaf_rats(town):
+def count_deaf_rats(town):
     return town.replace(' ', '')[::2].count('O')
