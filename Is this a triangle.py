8b58ffdc066602b2f01c759ccb5d7622a6f13995
Bowen Liu <45830436+BowenLiu92@users.noreply.github.com>
2019-12-04 10:04:23 -0500

    Update Is this a triangle.py

--- Is this a triangle.py
+++ Is this a triangle.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (default, Aug  9 2019, 18:34:13) [MSC v.1915 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def is_triangle(a, b, c):
+def is_triangle(a, b, c):
     l = [a, b, c]
     if all(e > 0 for e in l):
         maxVal = max(l)
